688a972509dea55ad25c43429e3ac5a5ddac065a
Oleg Tazetdinov <none@none>
2018-06-25 12:27:13 +0300

    Split into separate records of correct length.

--- broadlinkDecode.py
+++ broadlinkDecode.py
@@ -3,9 +3,20 @@ import binascii as ba
 
 f = io.FileIO('32194283', 'rb')
 data = f.readall()
-res = ba.hexlify(data[108:])
 
-fout = io.FileIO('result.txt', 'w')
-fout.write(res)
+fout = open('result.txt', 'w')
+
+offset = 108
+for i in range(501):
+  type = data[offset]
+  repeat = data[offset + 1]
+  length = data[offset + 2] + data[offset + 3] * 256
+  
+  record = ba.hexlify(data[offset + 4 : offset + 4 + length])
+  for elem in record:
+    fout.write('%c' % elem);
+
+  fout.write('\n')
+  offset += 960
 
 fout.close()
