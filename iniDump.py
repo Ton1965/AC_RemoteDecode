eac0277016de0471590eae0a11c897f1bafb3937
Oleg Tazetdinov <none@none>
2018-06-27 17:32:45 +0300

    Dump ini file from HASS plugin.

---
+++ iniDump.py
@@ -0,0 +1,36 @@
+import io
+import binascii as ba
+
+f = open('toyotomi_akira.ini', 'r')
+data = f.readlines()
+
+fout = open('toyotomi.hex.txt', 'w')
+
+for l in data:
+  offset = 0
+
+  if not '=' in l:
+#    fout.write(l)
+    continue
+  idx = l.find('=') + 1
+#  fout.write(l[:idx])
+  s = l[idx:]
+  s.strip()
+  data = ba.a2b_base64(s)
+  type = data[offset]
+  repeat = data[offset + 1]
+  length = data[offset + 2] + data[offset + 3] * 256
+  
+  record = ba.hexlify(data[offset + 4 : offset + 4 + length])
+  for elem in record:
+    fout.write('%c' % elem);
+  fout.write('\n')
+
+
+  offset = offset + 4 + length
+  if offset >= len(data):
+    print("Too long string!")
+    continue
+
+fout.close()
+
